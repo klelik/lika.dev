e5d59c8c5a8aa6cfc0ffd2c4dbdd2e4e913b37a8
Klement Lika <klement.lika@newmarketholidays.co.uk>
2025-04-24 22:57:36 +0100

    Add core styles, layout components 🌈

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,10 @@
 <template>
   <h1>Home</h1>
+  <h2>Home</h2>
+  <h3>Home</h3>
+  <h4>Home</h4>
+  <p>Home</p>
+  <p class="ts-label">Home</p>
+  <p class="ts-tiny">Home</p>
+  <p class="ts-micro">Home</p>
 </template>
